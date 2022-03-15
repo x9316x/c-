d2ae285030b030695579dd5aa3f607cd123a3f04
x9316x <x9316x@gmail.com>
2022-03-15 10:49:06 +0300

    change snake case with camel case

--- Program.cs
+++ Program.cs
@@ -75,52 +75,52 @@ First practical work. (Task 2, 4, 6, 8)
 // Task 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Ищем наибольшее из двух чисел.");
 Console.WriteLine("Введите первое число:");
-int task2_variable1 = Convert.ToInt32(Console.ReadLine());
+int task2Variable1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int task2_variable2 = Convert.ToInt32(Console.ReadLine());
+int task2Variable2 = Convert.ToInt32(Console.ReadLine());
 
-if(task2_variable1 > task2_variable2)
+if(task2Variable1 > task2Variable2)
 {
-   Console.WriteLine("Наибольшее из двух чисел " + task2_variable1 + " и " + task2_variable2 + " - это " + task2_variable1); 
+   Console.WriteLine("Наибольшее из двух чисел " + task2Variable1 + " и " + task2Variable2 + " - это " + task2Variable1); 
 }
 else
 {
-    Console.WriteLine("Наибольшее из двух чисел " + task2_variable1 + " и " + task2_variable2 + " - это " + task2_variable2); 
+    Console.WriteLine("Наибольшее из двух чисел " + task2Variable1 + " и " + task2Variable2 + " - это " + task2Variable2); 
 }
 
 // Task 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.WriteLine("Идём дальше!Теперь найдём наибольшее из трёх чисел.");
 
 Console.WriteLine("Введите первое число:");
-int task4_variable1 = Convert.ToInt32(Console.ReadLine());
+int task4Variable1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int task4_variable2 = Convert.ToInt32(Console.ReadLine());
+int task4Variable2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число:");
-int task4_variable3 = Convert.ToInt32(Console.ReadLine());
+int task4Variable3 = Convert.ToInt32(Console.ReadLine());
 
-if(task4_variable1 > task4_variable2)
+if(task4Variable1 > task4Variable2)
 {
-    if(task4_variable1 > task4_variable3)
+    if(task4Variable1 > task4Variable3)
     {
-        Console.WriteLine("Наибольшее из трёх чисел " + task4_variable1 + ", " + task4_variable2 + " и " + task4_variable3 + " - это " + task4_variable1); 
+        Console.WriteLine("Наибольшее из трёх чисел " + task4Variable1 + ", " + task4Variable2 + " и " + task4Variable3 + " - это " + task4Variable1); 
     }
     else
     {
-        Console.WriteLine("Наибольшее из трёх чисел " + task4_variable1 + ", " + task4_variable2 + " и " + task4_variable3 + " - это " + task4_variable3); 
+        Console.WriteLine("Наибольшее из трёх чисел " + task4Variable1 + ", " + task4Variable2 + " и " + task4Variable3 + " - это " + task4Variable3); 
     }
 }
 else
 {
-    if(task4_variable2 > task4_variable3)
+    if(task4Variable2 > task4Variable3)
     {
-        Console.WriteLine("Наибольшее из трёх чисел " + task4_variable1 + ", " + task4_variable2 + " и " + task4_variable3 + " - это " + task4_variable2); 
+        Console.WriteLine("Наибольшее из трёх чисел " + task4Variable1 + ", " + task4Variable2 + " и " + task4Variable3 + " - это " + task4Variable2); 
     }
     else
     {
-        Console.WriteLine("Наибольшее из трёх чисел " + task4_variable1 + ", " + task4_variable2 + " и " + task4_variable3 + " - это " + task4_variable3); 
+        Console.WriteLine("Наибольшее из трёх чисел " + task4Variable1 + ", " + task4Variable2 + " и " + task4Variable3 + " - это " + task4Variable3); 
     }
 }
 
@@ -130,28 +130,28 @@ Console.WriteLine("Отлично! Идём дальше!");
 Console.WriteLine("Определим, является ли число чётным:");
 
 Console.WriteLine("Введите число:");
-int task6_variable1 = Convert.ToInt32(Console.ReadLine());
+int task6Variable1 = Convert.ToInt32(Console.ReadLine());
 
-if(task6_variable1 % 2 == 0)
+if(task6Variable1 % 2 == 0)
 {
-    Console.WriteLine(task6_variable1 + " - чётное число.");
+    Console.WriteLine(task6Variable1 + " - чётное число.");
 }
 else
 {
-    Console.WriteLine(task6_variable1 + " - нечётное число.");
+    Console.WriteLine(task6Variable1 + " - нечётное число.");
 }
 
 // Task 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("Переходим к последнему заданию.");
 
 Console.WriteLine("Введите некоторое число N:");
-int task8_variable1 = Convert.ToInt32(Console.ReadLine());
+int task8Variable1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Покажем все чётные числа от 1 до " + task8_variable1);
+Console.WriteLine("Покажем все чётные числа от 1 до " + task8Variable1);
 
 int count = 2;
 
-while(count < task8_variable1)
+while(count < task8Variable1)
 {
     Console.Write(count + " ");
     count = count + 2;
